e30ab7a12bc075f3ad105c57c44bb3c023624410
NAJD ALEID <najdaleid6@gmail.com>
2024-02-16 22:22:50 -0800

    up

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((item, index) =>
+
+const { list } = require('./100-data');
+
+console.log('Initial list:', list);
+
+const newList = list.map((value, index) => value * index);
+
+console.log('Initial List:', initialList);
+
+console.log('New list:', newList);
